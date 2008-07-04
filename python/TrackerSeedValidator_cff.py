e17fd25ee4d6154779f9882df71b1ef25da984a2
Giuseppe Cerati <sha1-fba32708710cbf239b252728f58f96c6304a00dd@cern.ch>
2008-07-04 09:43:14 +0000

    use Configuration.StandardSequences.MagneticField_cff

--- python/TrackerSeedValidator_cff.py
+++ python/TrackerSeedValidator_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from MagneticField.Engine.volumeBasedMagneticField_cfi import *
+from Configuration.StandardSequences.MagneticField_cff import *
 from DQMServices.Core.DQM_cfg import *
 from Validation.RecoTrack.TrackerSeedValidator_cfi import *
 
