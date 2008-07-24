0245bb7f4e5cea84d2b0b01f6adcb073e15f24d7
Giuseppe Cerati <sha1-fba32708710cbf239b252728f58f96c6304a00dd@cern.ch>
2008-07-24 14:22:18 +0000

    include from PhysicsTools/RecoAlgos

--- python/cutsRecoTracks_cfi.py
+++ python/cutsRecoTracks_cfi.py
@@ -1,18 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 
-cutsRecoTracks = cms.EDFilter("RecoTrackSelector",
-    src = cms.InputTag('generalTracks'),
-    #default cuts are dummy: cut used to produce TDR plots are commented
-    ptMin = cms.double(0.1),
-    minRapidity = cms.double(-5.0), ##-2.5
-    maxRapidity = cms.double(5.0), ##2.5
-    tip = cms.double(120.0), ##3.5
-    lip = cms.double(300.0), ##30
-    minHit = cms.int32(3), ##8
-    maxChi2 = cms.double(10000.0), ##not used in tdr
-    quality = cms.string('loose'),
-    algorithm = cms.string('')
-
-)
-
-
+import PhysicsTools.RecoAlgos.recoTrackSelector_cfi
+cutsRecoTracks = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
