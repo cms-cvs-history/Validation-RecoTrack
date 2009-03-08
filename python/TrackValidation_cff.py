1d68f9b8bab2de1fea2f1567d1a3c5d2743e42d9
cmsbuild <>
2009-03-08 02:35:18 +0000

    This commit was manufactured by cvs2git to create tag 'Validation-RecoTrack- CMSSW_3_1_0_pre4'.
    Sprout from master 2009-02-03 14:35:59 UTC Chiara Genta <sha1-5a1ddac03dd3e96834db2f498eca6aad5396cb8e@cern.ch> 'fix histogram names'
    Cherrypick from master 2009-03-08 02:35:17 UTC Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch> 'fix the parameter types':
        interface/MultiTrackValidatorBase.h
        python/TrackValidation_cff.py

--- python/TrackValidation_cff.py
+++ python/TrackValidation_cff.py
@@ -2,8 +2,76 @@ import FWCore.ParameterSet.Config as cms
 
 from SimTracker.TrackAssociation.TrackAssociatorByChi2_cfi import *
 from SimTracker.TrackAssociation.TrackAssociatorByHits_cfi import *
-from Validation.RecoTrack.cuts_cff import *
 from Validation.RecoTrack.MultiTrackValidator_cfi import *
 from Validation.RecoTrack.PostProcessorTracker_cfi import *
-tracksValidation = cms.Sequence(multiTrackValidator)
+import PhysicsTools.RecoAlgos.recoTrackSelector_cfi
+
+# Validation iterative steps
+cutsRecoTracksFirst = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTracksFirst.algorithm=cms.vstring("ctf")
+
+cutsRecoTracksSecond = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTracksSecond.algorithm=cms.vstring("iter2")
+
+cutsRecoTracksThird = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTracksThird.algorithm=cms.vstring("iter3")
+
+cutsRecoTracksFourth = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTracksFourth.algorithm=cms.vstring("iter4")
+
+cutsRecoTracksFifth = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTracksFifth.algorithm=cms.vstring("iter5")
+
+# high purity
+cutsRecoTracksHp = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTracksHp.quality=cms.vstring("highPurity")
+
+cutsRecoTracksFirstHp = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTracksFirstHp.algorithm=cms.vstring("ctf")
+cutsRecoTracksFirstHp.quality=cms.vstring("highPurity")
+
+cutsRecoTracksSecondHp = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTracksSecondHp.algorithm=cms.vstring("iter2")
+cutsRecoTracksSecondHp.quality=cms.vstring("highPurity")
+
+cutsRecoTracksThirdHp = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTracksThirdHp.algorithm=cms.vstring("iter3")
+cutsRecoTracksThirdHp.quality=cms.vstring("highPurity")
+
+cutsRecoTracksFourthHp = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTracksFourthHp.algorithm=cms.vstring("iter4")
+cutsRecoTracksFourthHp.quality=cms.vstring("highPurity")
+
+cutsRecoTracksFifthHp = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTracksFifthHp.algorithm=cms.vstring("iter5")
+cutsRecoTracksFifthHp.quality=cms.vstring("highPurity")
+
+
+multiTrackValidator.label=cms.VInputTag(cms.InputTag("generalTracks"),
+                                        cms.InputTag("cutsRecoTracksHp"),
+                                        cms.InputTag("cutsRecoTracksFirst"),
+                                        cms.InputTag("cutsRecoTracksFirstHp"),
+                                        cms.InputTag("cutsRecoTracksSecond"),
+                                        cms.InputTag("cutsRecoTracksSecondHp"),
+                                        cms.InputTag("cutsRecoTracksThird"),
+                                        cms.InputTag("cutsRecoTracksThirdHp"),
+                                        cms.InputTag("cutsRecoTracksFourth"),
+                                        cms.InputTag("cutsRecoTracksFourthHp"),
+                                        cms.InputTag("cutsRecoTracksFifth"),
+                                        cms.InputTag("cutsRecoTracksFifthHp")
+                                        )
+
+
+tracksValidation = cms.Sequence(cutsRecoTracksHp*
+                                cutsRecoTracksFirst*
+                                cutsRecoTracksFirstHp*
+                                cutsRecoTracksSecond*
+                                cutsRecoTracksSecondHp*
+                                cutsRecoTracksThird*
+                                cutsRecoTracksThirdHp*
+                                cutsRecoTracksFourth*
+                                cutsRecoTracksFourthHp*
+                                cutsRecoTracksFifth*
+                                cutsRecoTracksFifthHp*
+                                multiTrackValidator)
 
